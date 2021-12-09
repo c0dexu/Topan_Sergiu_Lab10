190e5dada53bff23a5a3a5ed0baf0fb4664a73f9
Topan Sergiu <sergiu.smw@gmail.com>
2021-12-09 16:37:14 +0200

    a

---
+++ Topan_Sergiu_Lab10/Models/ShopList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+
+namespace Topan_Sergiu_Lab10.Models
+{
+    class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
