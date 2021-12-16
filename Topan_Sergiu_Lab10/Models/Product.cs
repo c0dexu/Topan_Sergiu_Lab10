43a7bdaede3075fca4759806b2fa7c88254a9701
Topan Sergiu <sergiu.smw@gmail.com>
2021-12-16 14:35:04 +0200

    a

---
+++ Topan_Sergiu_Lab10/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Topan_Sergiu_Lab10.Models
+{
+    class Product
+    {
+    }
+}
