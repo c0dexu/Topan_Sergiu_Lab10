570c05cfe063856652f5f2280d9e039b2d8a5dff
Topan Sergiu <sergiu.smw@gmail.com>
2022-01-20 22:03:55 +0200

    test

---
+++ Topan_Sergiu_Lab10/Models/test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topan_Sergiu_Lab10.Models
+{
+    class test
+    {
+        test() { }
+        public void hello()
+        {
+
+        }
+        
+
+        
+    }
+}
